784ee4ab3a4588a24dfdc48d1990518016eb17e4
Newman-99 <newmanrodriguez1999@gmail.com>
2019-10-18 15:53:16 -0400

    Newman: borro  Archivos demo.html y dashborad1.php por ser innecesarios

--- public/register_docent.php
+++ public/register_docent.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Registro de Docentes</title>
 </head>
 <body>
-    <h3>Esto</h3>
+
+    <h2>Registro de Docentes</h2>
+    <?php ?>
+
+    <form action="" method="post"></form>
 </body>
 </html>
